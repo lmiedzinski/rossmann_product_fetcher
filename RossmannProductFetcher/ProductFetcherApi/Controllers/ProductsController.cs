fc9962f1dc2ca0747470232a0ae8ec10bba68be2
Łukasz Miedziński <lukasz@miedzinski.org>
2019-01-03 20:32:48 +0100

    Controller renamed

---
+++ RossmannProductFetcher/ProductFetcherApi/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductFetcherApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/products/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
