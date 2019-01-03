9607818202b4648e33a71b1fc7540ba79e05ca85
Łukasz Miedziński <lukasz@miedzinski.org>
2019-01-03 20:55:22 +0100

    Created ProductRepository

---
+++ RossmannProductFetcher/ProductFetcherApi/Repositories/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductFetcherApi.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<BsonDocument> GetProductById(int id);
+        Task<BsonDocument> AddProduct(string productData);
+    }
+}
---
+++ RossmannProductFetcher/ProductFetcherApi/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using ProductFetcherApi.DbConnectors;
+
+namespace ProductFetcherApi.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private const string PRODUCTS_COLLECTION_NAME = "products";
+        private readonly IMongoCollection<BsonDocument> _products;
+
+        public ProductRepository(MongoDbConnector mongoDbConnector)
+        {
+            _products = mongoDbConnector.Database.GetCollection<BsonDocument>(PRODUCTS_COLLECTION_NAME);
+        }
+
+        public async Task<BsonDocument> AddProduct(string productData)
+        {
+            var document = BsonSerializer.Deserialize<BsonDocument>(productData);
+            await _products.InsertOneAsync(document);
+            return document;
+        }
+
+        public async Task<BsonDocument> GetProductById(int id)
+        {
+            return await _products.Find(x => x.GetValue("data").AsBsonDocument.GetValue("id").AsInt32 == id).FirstAsync();
+        }
+    }
+}
